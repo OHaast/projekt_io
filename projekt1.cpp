c23b28d9fd0c719b377f0d12c0e1475a6b73419f
Olivier Hasterok <lponon3787@gmail.com>
2026-06-08 00:12:39 +0200

    Dodanie obslugi liczby pierwszej w menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -23,10 +23,36 @@ bool czyPierwsza(int n)
 int main()
 {
     int wybor;
+    int liczba;
 
     cout << "MENU\n";
+    cout << "1. Sprawdz liczbe pierwsza\n";
     cout << "0. Wyjscie\n";
+
     cin >> wybor;
+
+    switch(wybor)
+    {
+        case 1:
+            cout << "Podaj liczbe: ";
+            cin >> liczba;
+
+            if(czyPierwsza(liczba))
+            {
+                cout << "Liczba jest pierwsza\n";
+            }
+            else
+            {
+                cout << "Liczba nie jest pierwsza\n";
+            }
+            break;
+
+        case 0:
+            break;
+
+        default:
+            cout << "Niepoprawny wybor\n";
+    }
 
     return 0;
 }
